ad1af20dfac619e0f012a95257f28ab010790925
seominjeong <smj12938@gmail.com>
2022-07-09 23:36:31 +0900

    add category page

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import axios from "axios";
 import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
 
 const App = () => {
-  return <NewsList />;
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
   // const [data, setData] = useState(null);
   // const onClick = async () => {
   //   try {
