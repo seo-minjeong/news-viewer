274ff6d7ead454d043fc12e110a8d6bb163ec292
seominjeong <smj12938@gmail.com>
2022-07-11 00:20:20 +0900

    hooks process

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import NewsPage from "./pages/NewsPage";
 
 const App = () => {
+  // ?는 값이 선택적이라는 의미. url 파라미터가 없다면 전체 카테고리를 선택한 것으로 간주.
   return <Route path="/:category?" component={NewsPage} />;
 };
 
